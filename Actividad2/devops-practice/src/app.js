d503ea243a67328b35bb83ae22515d3e54bfa513
ivanurbanodev <ivan.urbano.n@uni.pe>
2024-09-12 00:19:01 -0500

    actividad2

---
+++ Actividad2/devops-practice/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Solo inicia el servidor si este archivo se está ejecutando directamente
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+module.exports = app;
---
+++ Actividad2/devops-practice/tests/app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /', () => {
+  let server;
+
+  beforeAll(() => {
+    server = app.listen(0); // Usar 0 permite que el sistema asigne un puerto libre
+  });
+
+  afterAll((done) => {
+    server.close(done); // Cierra el servidor después de las pruebas
+  });
+
+  it('should return Hello, World!', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello, World!');
+  });
+});
